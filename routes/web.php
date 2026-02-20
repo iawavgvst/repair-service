6345fa9ab9c8ce58b82b8bece2d582f77c425f23
iawavgvst <alkelin1@g,ail.com>
2026-02-20 13:04:19 +0300

    routes (master, dispatcher)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DispatcherController;
+use App\Http\Controllers\MasterController;
+use App\Http\Controllers\RepairRequestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -11,5 +14,33 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.action');
 
-// выход из системы
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// создание заявок
+Route::prefix('requests')->group(function () {
+    Route::get('/create', [RepairRequestController::class, 'create'])->name('requests.create');
+    Route::post('/', [RepairRequestController::class, 'store'])->name('requests.store');
+});
+
+// для аутентифицированных пользователей
+Route::middleware(['auth'])->group(function () {
+    // выход из системы
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    // для диспетчера
+    Route::prefix('dispatcher')->group(function () {
+        Route::get('/', [DispatcherController::class, 'index'])->name('dispatcher.index');
+        Route::get('/{repairRequest}', [DispatcherController::class, 'show'])->name('dispatcher.show');
+
+        Route::patch('/{repairRequest}/assign', [DispatcherController::class, 'assignMaster'])->name('dispatcher.assign');
+        Route::patch('/{repairRequest}/cancel', [DispatcherController::class, 'cancelRequest'])->name('dispatcher.cancel');
+    });
+
+    // для мастера
+    Route::prefix('master')->group(function () {
+        Route::get('/', [MasterController::class, 'index'])->name('master.index');
+        Route::get('/{repairRequest}', [MasterController::class, 'show'])->name('master.show');
+
+        Route::patch('/{repairRequest}/take', [MasterController::class, 'takeRequest'])->name('master.take');
+        Route::patch('/{repairRequest}/complete', [MasterController::class, 'completeRequest'])->name('master.complete');
+    });
+});
+
